4f56cb9c4b53bb738026e527b2a674454933185b
nori <norinorimcz@gmail.com>
2021-08-10 16:12:56 +0900

    不要なルーチンの削除

--- ast2js/src/util/boolutil.py
+++ ast2js/src/util/boolutil.py
@@ -21,31 +21,3 @@ def deep_get(d, keys, default=None):
         if len(d) > keys[0]:
             next = d[keys[0]]
     return deep_get(next, keys[1:], default)
-
-
-"""
-bool値をオブジェクト指向的に操作する簡易関数
-"""
-class Boolean:
-    def __init__(self, value=None):
-        if value is not None:
-            self._value = value
-        return
-    
-    def set_value(self, value: bool):
-        self._value = value
-        return
-    
-    def toggle(self):
-        self._value = not self._value
-
-    def get(self):
-        return self._value
-    
-    def update(self, mode: str, aCondition):
-        if mode == 'and':
-            self._value &= aCondition
-        elif mode == 'or':
-            self._value |= aCondition
-        if mode == 'xor':
-            self._value ^= aCondition
